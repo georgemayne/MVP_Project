3f4c01eff704ed9cbaa5b3d030610bcf14666872
georgemayne <georgeegyirmayne07@gmail.com>
2024-02-19 23:04:47 +0000

    amend

---
+++ js/swiper.js
@@ -0,0 +1,20 @@
+
+
+const swiper = new Swiper('.sw-image', {
+  // Optional parameters
+  slidesPerView: 1,
+  loop: 1,
+
+  //pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
